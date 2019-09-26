decd282aa9f8c0690ef5bde78433c9470d2a2a16
Oleg <debagull@gmail.com>
2019-09-26 17:25:23 +0300

    todo version2

--- ToDo/add.js
+++ ToDo/add.js
@@ -1,105 +1,100 @@
+const inputOnFocus = document.getElementById('txt');
 let checkCheked = document.getElementsByClassName('todoinput');
 let listSpan = document.getElementsByClassName('editspan');
 let blockDone = document.getElementById('block-done');
-let block1 = document.getElementById('block-1').innerHTML;
+let block1 = document.getElementById('block-1');
 let statusToDo = document.getElementById('statusbar-todo');
 let statusDone = document.getElementById('statusbar-done');
 let maxWidth;
 let widthBar = 0;
-let number = 0;
+let todoList = [];
 
-document.getElementById('block-done').innerHTML = localStorage.getItem('block-done');
-document.getElementById('block-1').innerHTML = localStorage.getItem('block-1');
+function generateId() {
+  return `f${(+new Date).toString(16)}`
+}
+
 statusToDo.innerHTML = localStorage.getItem('width1');
 statusDone.innerHTML = localStorage.getItem('width2');
-// function checkOk() {
-//   if (blockDone) {
-//     for (i = 0; i < blockDone.children.length; i++){
-//       console.log(blockDone.children[i]);
-//       blockDone.children[i].querySelectorAll('input').setAttribute('checked', 'checked');
-//     }
-//   }
-// }
-// checkOk();
-// let todoItem = {
-//  text: '',
-//  id: '',
-//  status: ''
-// }
-
-// [todoItem, todoItem, todoItem].forEach(viewElem)
-
-// function viewElem(todo) {
-//   let view = `<li id=${todo.id}>
-//                 <span>${todo.text}</span>
-//                 <input type="checkbox" ${ todo.status && "checked"}>
-//               </li>`
-
-//   if (todo.status) blockDone.innerHTML(view);
-//   else block1(view)
-// }
-
-// [].find(todo => if (todo.id === liId) todo.status = true)
-
-function createLi(){
-	const element = document.getElementById('block-1');
-	const listElem = document.createElement('li');
-	const input = document.getElementById('txt').value;
-
-	listElem.className = 'list';
-	listElem.innerHTML = '<input type="checkbox" class="todoinput"><span class="editspan">' + input + 
-	'</span><img class="delete" id="delete' + String(number) + '" src="img.gif">';
-	number++;
-
-	element.appendChild(listElem);
-
-  localStorage.setItem('block-1', document.getElementById('block-1').innerHTML);
-  localStorage.setItem('block-done', document.getElementById('block-done').innerHTML);
-
-  listing();
-  dblSpan();
-  deleteLi();
-  progressNum();
-
-
+if (localStorage.getItem('todo')){
+  todoList = JSON.parse(localStorage.getItem('todo'));
 }
 
-const inputOnFocus = document.getElementById('txt');
+function sortTodo(){
+  for (i = 0; i < todoList.length; i++){
+    let key = todoList[i];
+    let input = key.todo;
+    let value = key.check;
+    let id = key.id;
+    const element =`<li class="list">
+                      <input type="checkbox" class="todoinput" id="${id}" value="${value}">
+                      <span class="editspan">${input}</span>
+                      <img class="delete" src="img.gif">
+                    </li>`;
+    if (!key.check) {
+      block1.insertAdjacentHTML('beforeEnd', element); 
+    } else {
+      blockDone.insertAdjacentHTML('beforeEnd', element);
+      document.getElementById(`${key.id}`).setAttribute('checked', 'checked');
+    }               
+  }
+}
+sortTodo();
+
+
 inputOnFocus.onkeyup = function (e) {
-    e = e || window.event;
-    if (e.keyCode === 13 && inputOnFocus.value === ''){
-    	alert('Did you forget to type your item?');
-      return;
-    }
-
-    if (e.keyCode === 13 && inputOnFocus.value) {
-        createLi();
-        inputOnFocus.value = null;
-        inputOnFocus.focus();
-    }
-    return false;
+  e = e || window.event;
+  if (e.keyCode === 13 && inputOnFocus.value === ''){
+  	alert('Did you forget to type your item?');
+    return;
+  }
+  if (e.keyCode === 13 && inputOnFocus.value) {
+    let temp = {};
+    temp.todo = inputOnFocus.value;
+    temp.check = false;
+    temp.id = generateId();
+    let i = todoList.length;
+    todoList[i] = temp;
+    const element =`<li class="list">
+                      <input type="checkbox" class="todoinput" id="${temp.id}" value="${temp.check}">
+                      <span class="editspan">${inputOnFocus.value}</span>
+                      <img class="delete" src="img.gif">
+                    </li>`;
+    block1.insertAdjacentHTML('beforeEnd', element);
+    inputOnFocus.value = null;
+    inputOnFocus.focus();
+    listing();
+    dblSpan();
+    deleteLi();
+    progressNum();
+    localStorage.setItem('todo', JSON.stringify(todoList));
+  }
+  return false;
 }
 
-function listing() {
+function listing(e) {
+  e = e || window.event;
   for (let i = 0; i < checkCheked.length; i++) {
     checkCheked[i].onclick = function() {
       checkChekedNew = checkCheked[i];
       if (checkChekedNew.checked) {
+          let idCheck = todoList.find( elem => elem.id === checkChekedNew.id);
+          idCheck.check = true;
           checkChekedNew.setAttribute('checked', 'checked');
           document.getElementById('block-done').appendChild(checkChekedNew.parentElement);
           progressNum();
+          localStorage.setItem('todo', JSON.stringify(todoList));
           return listing();
       } else {
+          let idCheck = todoList.find( elem => elem.id === checkChekedNew.id);
+          idCheck.check = false;
           checkChekedNew.removeAttribute('checked');
           document.getElementById('block-1').appendChild(checkChekedNew.parentElement);
+          localStorage.setItem('todo', JSON.stringify(todoList));
           progressNum();
           return listing();
       }
     }
-
   }
-  localStorage.setItem('block-1', document.getElementById('block-1').innerHTML);
-  localStorage.setItem('block-done', document.getElementById('block-done').innerHTML);
 }
 
 function dblSpan() {
@@ -107,12 +102,13 @@ function dblSpan() {
 	  let target = listSpan[i];
 	  target.ondblclick = function() {
 	  	if (target.children.length < 1){
-	  		let defaultValue = target.innerText; 
+	  		let defaultValue = target.innerText;
+        let todoValue = todoList.find( elem => elem.id === target.parentElement.firstElementChild.id);
 	  		let buttonNum = String(document.getElementsByClassName('save-button').length);
 		  	target.innerHTML = '<input type="text" class="redact" value="' + target.innerText + 
 			  	'"><button id="save' + buttonNum + '" class="save-button">save</button><button id="cancel' +
 			  	buttonNum + '" class="cancel-button">cancel</button>';
-		  	buttonClick(target, buttonNum, defaultValue);
+		  	buttonClick(target, buttonNum, defaultValue, todoValue);
 	  	} else {
 	  		return;
 	  	}
@@ -121,19 +117,17 @@ function dblSpan() {
 }
 
 
-function buttonClick(target, buttonNum, defaultValue) {
+function buttonClick(target, buttonNum, defaultValue, todoValue) {
 	const buttonCancel = document.getElementById('cancel' + buttonNum);
 	const buttonSave = document.getElementById('save' + buttonNum);
 	buttonSave.onclick = function(){
 		target.innerHTML = target.firstChild.value;
-    localStorage.setItem('block-1', document.getElementById('block-1').innerHTML);
-    localStorage.setItem('block-done', document.getElementById('block-done').innerHTML);
+    todoValue.todo = target.innerHTML;
+    localStorage.setItem('todo', JSON.stringify(todoList));
 	}
 
 	buttonCancel.onclick = function(){
 		target.innerHTML = defaultValue;
-    localStorage.setItem('block-1', document.getElementById('block-1').innerHTML);
-    localStorage.setItem('block-done', document.getElementById('block-done').innerHTML);
 	}
 }
 
@@ -149,36 +143,36 @@ function progressNum() {
 	}
 }
 
-
-function move() {
-  let elem = document.getElementById('statusbar-done');
+function move(){
   let id = setInterval(frame, 16);
-  function frame() {
+  function frame(){
     if (widthBar === maxWidth) {
       clearInterval(id);
     } else if (widthBar < maxWidth) {
       widthBar++;
-      elem.style.width = widthBar + '%';
+      statusDone.style.width = widthBar + '%';
       statusToDo.style.width = widthBar + '%';
     } else if (widthBar > maxWidth) {
       widthBar--;
-      elem.style.width = widthBar + '%';
+      statusDone.style.width = widthBar + '%';
       statusToDo.style.width = widthBar + '%';
-      localStorage.setItem('width1', statusToDo.innerHTML);
-      localStorage.setItem('width2', elem.innerHTML);
-    }
+    }      
+    localStorage.setItem('width1', statusToDo.innerHTML);
+    localStorage.setItem('width2', statusDone.innerHTML);
   }
 }
 
-const elementLi = document.getElementsByClassName('list');
+
 function deleteLi() {
+  const elementLi = document.getElementsByClassName('list');
 	for(i = 0; i < elementLi.length; i++) {
 		const deleteThisLi = elementLi[i];
-		deleteThisLi.lastChild.onclick = function() {
-			deleteThisLi.remove();
+    const todoDelete = todoList.findIndex( elem => elem.id === deleteThisLi.children[0].id);
+		deleteThisLi.children[2].onclick = function() {
+      deleteThisLi.remove();
+      todoList.splice(todoDelete, 1);
+      localStorage.setItem('todo', JSON.stringify(todoList));
 			listing();
-      localStorage.setItem('block-1', document.getElementById('block-1').innerHTML);
-      localStorage.setItem('block-done', document.getElementById('block-done').innerHTML);
       progressNum();
 			return deleteLi();
 		}
@@ -188,9 +182,3 @@ listing();
 dblSpan();
 deleteLi();
 progressNum();
-
-
-
-
-
-
